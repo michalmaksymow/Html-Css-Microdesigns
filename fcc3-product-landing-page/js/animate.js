6c36a8f1229f2aa8958b07f46aa6f68d24894cac
Michał Maksymów <borysbrat@gmail.com>
2021-05-02 16:56:49 +0200

    Update about us animations

--- fcc3-product-landing-page/js/animate.js
+++ fcc3-product-landing-page/js/animate.js
@@ -10,12 +10,24 @@
     if (scrollPosVH >= sectionAboutUs.offsetTop + 200) {
       animate1();
     }
+    if (scrollPosVH >= sectionAboutUs.offsetTop + 500) {
+      animate2();
+    }
   });
 
   let animated1 = false;
   const animate1 = () => {
     if (!animated1) {
       document.querySelector(".js--animate-1").classList.add("anim__move-left-1");
+      animated1 = true;
+    }
+  };
+
+  let animated2 = false;
+  const animate2 = () => {
+    if (!animated2) {
+      document.querySelector(".js--animate-2").classList.add("anim__move-left-1");
+      animated2 = true;
     }
   };
 })();
