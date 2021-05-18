991e0d56e8acb0b0758430c48e199a4074364853
Michał Maksymów <borysbrat@gmail.com>
2021-05-18 11:08:22 +0200

    Add FCC tests | Technical documentation

--- fcc-4-technical-documentation/js/app.js
+++ fcc-4-technical-documentation/js/app.js
@@ -2,10 +2,7 @@ const documentation = document.getElementById("main-doc");
 const navLinks = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll(".main-section");
 
-sections.forEach((section) => {
-  console.log(section.getAttribute("id"), section.getBoundingClientRect());
-});
-
 documentation.addEventListener("scroll", (e) => {
   console.log(documentation.scrollTop);
 });
+// TODO update navbarlinks on scroll
