8c72c7cc468cb959ef8aa75f062a28c5d2f60c81
Michał Maksymów <borysbrat@gmail.com>
2021-05-18 12:18:46 +0200

    Add nav positioning on scroll

--- fcc-4-technical-documentation/js/app.js
+++ fcc-4-technical-documentation/js/app.js
@@ -2,7 +2,39 @@ const documentation = document.getElementById("main-doc");
 const navLinks = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll(".main-section");
 
-documentation.addEventListener("scroll", (e) => {
-  console.log(documentation.scrollTop);
+const updatePosition = function () {
+  let highestInView;
+  for (const [_, section] of sections.entries()) {
+    if (section.inView) {
+      highestInView = section;
+      break;
+    }
+  }
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${highestInView.getAttribute("id")}`) {
+      link.classList.add("active");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target["inView"] = true;
+      } else {
+        entry.target["inView"] = false;
+      }
+    });
+    updatePosition();
+  },
+  {
+    root: null,
+    threshold: 0,
+  }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
 });
-// TODO update navbarlinks on scroll
