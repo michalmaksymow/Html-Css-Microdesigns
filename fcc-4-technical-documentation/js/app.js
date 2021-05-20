aff6224cfb4848c24ee30e3b936ee3546e6c8155
Michał Maksymów <borysbrat@gmail.com>
2021-05-20 16:06:41 +0200

    Update navbar animation

--- fcc-4-technical-documentation/js/app.js
+++ fcc-4-technical-documentation/js/app.js
@@ -1,6 +1,8 @@
 const documentation = document.getElementById("main-doc");
 const navLinks = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll(".main-section");
+const navbarToggleBtn = document.querySelector(".navbar-toggle");
+const navbarList = document.querySelector(".navbar-list");
 
 const updatePosition = function () {
   let highestInView;
@@ -38,3 +40,66 @@ const observer = new IntersectionObserver(
 sections.forEach((section) => {
   observer.observe(section);
 });
+
+// NAVBAR ANIMATIONS
+const [iconTimes, iconBars] = ["fa-times", "fa-bars"];
+
+navbarToggleBtn.addEventListener("click", (e) => {
+  const icon = navbarToggleBtn.querySelector(".fas");
+  const isCollapsed = navbarList.getAttribute("data-collapsed") === "true";
+  if (!isCollapsed) {
+    icon.classList.remove(iconTimes);
+    icon.classList.add(iconBars);
+    collapseSection(navbarList);
+  } else {
+    icon.classList.remove(iconBars);
+    icon.classList.add(iconTimes);
+    expandSection(navbarList);
+  }
+});
+
+const collapseSection = function (element) {
+  // get the height of the element's inner content, regardless of its actual size
+  const sectionHeight = element.scrollHeight;
+
+  // temporarily disable all css transitions
+  const elementTransition = element.style.transition;
+  element.style.transition = "";
+
+  // on the next frame (as soon as the previous style change has taken effect),
+  // explicitly set the element's height to its current pixel height, so we
+  // aren't transitioning out of 'auto'
+  requestAnimationFrame(() => {
+    element.style.height = sectionHeight + "px";
+    element.style.transition = elementTransition;
+
+    // on the next frame (as soon as the previous style change has taken effect),
+    // have the element transition to height: 0
+    requestAnimationFrame(function () {
+      element.style.height = 0 + "px";
+    });
+  });
+
+  // mark the section as "currently collapsed"
+  element.setAttribute("data-collapsed", "true");
+};
+
+expandSection = function (element) {
+  // get the height of the element's inner content, regardless of its actual size
+  const sectionHeight = element.scrollHeight;
+
+  // have the element transition to the height of its inner content
+  element.style.height = sectionHeight + "px";
+
+  // when the next css transition finishes (which should be the one we just triggered)
+  element.addEventListener("transitionend", function (e) {
+    // remove this event listener so it only gets triggered once
+    element.removeEventListener("transitionend", arguments.callee);
+
+    // remove "height" from the element's inline styles, so it can return to its initial value
+    element.style.height = null;
+  });
+
+  // mark the section as "currently not collapsed"
+  element.setAttribute("data-collapsed", "false");
+};
